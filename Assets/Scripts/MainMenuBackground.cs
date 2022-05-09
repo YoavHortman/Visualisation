0199c523406fdda775c1c452c27e8b7e7ef3157e
yoavhortman <yoav.hortman@moixa.com>
2022-05-09 20:31:12 +0100

    working circle

--- Assets/Scripts/MainMenuBackground.cs
+++ Assets/Scripts/MainMenuBackground.cs
@@ -4,10 +4,24 @@ using System.Linq;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
-struct SpriteWithMetedata {
-  private Sprite sprite;
-  private Vector2 sizeData;
-  private Color colorData;
+class SpriteWithMetedata {
+  public Sprite sprite;
+  public Vector2 sizeData;
+  public SpriteRenderer spriteRenderer; 
+  public SpriteWithMetedata(Sprite sprite, Vector2 sizeData, bool leaving) {
+    this.sprite = sprite;
+    this.sizeData = sizeData;
+  }
+}
+
+class Instance {
+  public bool leaving;
+  public SpriteRenderer spriteRenderer;
+
+  public Instance(SpriteRenderer spriteRenderer, bool leaving) {
+    this.spriteRenderer = spriteRenderer;
+    this.leaving = leaving;
+  }
 }
 
 enum Patterns {
@@ -27,8 +41,8 @@ public class MainMenuBackground : MonoBehaviour {
   [SerializeField] private int orderInLayer = 0;
 
   private float borderPadding = 0f;
-  private Tuple<Sprite, Vector2>[] _spritesWithMetaData;
-  private SpriteRenderer[] instances;
+  private SpriteWithMetedata[] _spritesWithMetaData;
+  private Instance[] instances;
   private Camera mainCam;
   private int rowCount;
   private int colCount;
@@ -50,7 +64,7 @@ public class MainMenuBackground : MonoBehaviour {
 
   [SerializeField] Childrens[] Combos;
 
-  Tuple<Sprite, Vector2> getRandomSpriteWithMeteData() {
+  SpriteWithMetedata getRandomSpriteWithMeteData() {
     if (overrideRandomWith != null) {
       return getMetadata(overrideRandomWith[Random.Range(0, overrideRandomWith.Length)]);
     }
@@ -58,8 +72,8 @@ public class MainMenuBackground : MonoBehaviour {
     return _spritesWithMetaData[Random.Range(0, _spritesWithMetaData.Length)];
   }
 
-  Tuple<Sprite, Vector2> getMetadata(Sprite sprite) {
-    return _spritesWithMetaData.First(x => x.Item1 == sprite);
+  SpriteWithMetedata getMetadata(Sprite sprite) {
+    return _spritesWithMetaData.First(x => x.sprite == sprite);
   }
 
   float getRandomInRange() {
@@ -68,7 +82,7 @@ public class MainMenuBackground : MonoBehaviour {
 
 
   void Start() {
-    instances = new SpriteRenderer[] { };
+    instances = new Instance[] { };
     mainCam = Camera.main;
     _grid = GetComponent<Grid>();
     UpdateGrid();
@@ -95,28 +109,48 @@ public class MainMenuBackground : MonoBehaviour {
   [EditorButton]
   void UpdateSize() {
     SetPattern();
-    if (pattern == Patterns.STEPS) {
-      var rowCol = new Tuple<int, int>(1,1);
-      while (rowCol.Item1 % 2 != 0 || rowCol.Item2 % 2 != 0) {
-        spriteSize = Random.Range(0.1f, 1f);
-        borderPadding = spriteSize / 2;
-        rowCol = GetNextColAndRow(ResizeListener.screenSizeInWorldCoords);
+    switch (pattern) {
+        case Patterns.DEFAULT:
+        case Patterns.DIAGONAL:
+        case Patterns.ZIGZAG: {
+          spriteSize = Random.Range(0.1f, 10f);
+          borderPadding = Random.Range(0f, 0.3f);
+          break;
+        }
+        case Patterns.STEPS: {
+          var rowCol = new Tuple<int, int>(1,1);
+          while (rowCol.Item1 % 2 != 0 || rowCol.Item2 % 2 != 0) {
+            spriteSize = Random.Range(0.1f, 1f);
+            borderPadding = spriteSize / 2;
+            rowCol = GetNextColAndRow(ResizeListener.screenSizeInWorldCoords);
+          }
+          break;
+        }
+        case Patterns.CIRCLE: {
+          // TODO have it so it can handle odd numbers.
+          var rowCol = new Tuple<int, int>(1,1);
+          while (rowCol.Item1 % 2 != 0 || rowCol.Item2 % 2 != 0) {
+            spriteSize = Random.Range(0.1f, 1f);
+            borderPadding = spriteSize / 2;
+            rowCol = GetNextColAndRow(ResizeListener.screenSizeInWorldCoords);
+          }
+          break;
+        }
+        default:
+          throw new Exception("unhandled switch case");
       }
-    } else {
-      spriteSize = Random.Range(0.1f, 10f);
-      borderPadding = Random.Range(0f, 0.3f);
-    }
   
     UpdateGrid();
     InitSpritesWithMetaData();
-    Invoke(nameof(UpdateSize), getRandomInRange());
+    // Invoke(nameof(UpdateSize), getRandomInRange());
   }
 
+  [EditorButton]
   void UpdateSpeedAndRotation() {
     RotationSpeed = Random.Range(-50f, 50f);
     MovementSpeedX = Random.Range(-2f, 2f);
     MovementSpeedY = Random.Range(-2f, 2f);
-    Invoke(nameof(UpdateSpeedAndRotation), getRandomInRange());
+    // Invoke(nameof(UpdateSpeedAndRotation), getRandomInRange());
   }
 
 
@@ -133,12 +167,12 @@ public class MainMenuBackground : MonoBehaviour {
   }
 
   void InitSpritesWithMetaData() {
-    _spritesWithMetaData = new Tuple<Sprite, Vector2>[sprites.Length];
+    _spritesWithMetaData = new SpriteWithMetedata[sprites.Length];
 
     for (var i = 0; i < _spritesWithMetaData.Length; i++) {
       var sprite = sprites[i];
       float ratio = spriteSize / Mathf.Sqrt(sprite.bounds.size.x * sprite.bounds.size.x + sprite.bounds.size.y * sprite.bounds.size.y);
-      _spritesWithMetaData[i] = new Tuple<Sprite, Vector2>(sprite, new Vector2(sprite.bounds.size.x * ratio, sprite.bounds.size.y * ratio));
+      _spritesWithMetaData[i] = new SpriteWithMetedata(sprite, new Vector2(sprite.bounds.size.x * ratio, sprite.bounds.size.y * ratio), true);
     }
 
     AfterResize(ResizeListener.screenSizeInWorldCoords);
@@ -150,7 +184,7 @@ public class MainMenuBackground : MonoBehaviour {
       if (Random.value > 0.5f && Combos.Length > 0) {
         overrideRandomWith = Combos[Random.Range(0, Combos.Length)].childrens.ToArray();
       } else {
-        overrideRandomWith = new[] {getRandomSpriteWithMeteData().Item1};
+        overrideRandomWith = new[] {getRandomSpriteWithMeteData().sprite};
       }
     } else {
       overrideRandomWith = null;
@@ -167,15 +201,28 @@ public class MainMenuBackground : MonoBehaviour {
   void ChangeColor() {
     color = Color.Lerp(color, targetColor, Time.deltaTime);
     foreach (var i in instances) {
-      i.color = color;
+      i.spriteRenderer.color = color;
     }
   }
 
   Tuple<int, int> GetNextColAndRow(Vector2 screenSizeInWorldCoords) {
     var nextColCount = Mathf.CeilToInt(screenSizeInWorldCoords.x * 2 / GetFullSize());
     var nextRowCount = Mathf.CeilToInt(screenSizeInWorldCoords.y * 2 / GetFullSize());
-    nextColCount++;
-    nextRowCount++;
+    switch (pattern) {
+        case Patterns.DEFAULT:
+        case Patterns.DIAGONAL:
+        case Patterns.ZIGZAG:
+        case Patterns.STEPS: {
+          nextColCount++;
+          nextRowCount++;
+          break;
+        }
+        case Patterns.CIRCLE: {
+          break;
+        }
+        default:
+          throw new Exception("unhandled switch case");
+      }
     return new Tuple<int, int>(nextColCount, nextRowCount);
   }
 
@@ -189,24 +236,24 @@ public class MainMenuBackground : MonoBehaviour {
 
     if (nextSize < instances.Length) {
       for (var i = nextSize; i < instances.Length; i++) {
-        Destroy(instances[i].gameObject);
+        Destroy(instances[i].spriteRenderer.gameObject);
       }
     }
-
-    var newInstances = new SpriteRenderer[colCount * rowCount];
+    var newInstances = new Instance[colCount * rowCount];
     for (var i = 0; i < newInstances.Length; i++) {
       if (instances.Length > i) {
         newInstances[i] = instances[i];
-        var sr = instances[i].GetComponent<SpriteRenderer>();
-        sr.size = getMetadata(sr.sprite).Item2;
+        var sr = instances[i].spriteRenderer.GetComponent<SpriteRenderer>();
+        sr.size = getMetadata(sr.sprite).sizeData;
       } else {
-        newInstances[i] = new GameObject("Sprite" + i).AddComponent<SpriteRenderer>();
-        newInstances[i].transform.parent = transform;
-        newInstances[i].color = color;
+        newInstances[i] = new Instance(new GameObject("Sprite" + i).AddComponent<SpriteRenderer>(), false);
+        newInstances[i].spriteRenderer.transform.parent = transform;
+        newInstances[i].spriteRenderer.color = color;
       }
     }
 
-    instances = new SpriteRenderer[colCount * rowCount];
+
+    instances = new Instance[colCount * rowCount];
     instances = newInstances;
 
     var initialPos = mainCam.ViewportToWorldPoint(new Vector3(0, 0, mainCam.nearClipPlane));
@@ -214,9 +261,9 @@ public class MainMenuBackground : MonoBehaviour {
     var currCol = 0;
     var currRow = 0;
     foreach (var instance in newInstances) {
-      ConfigSpriteRenderer(instance);
-      instance.transform.position = new Vector3(currCol * GetFullSize() + initialPos.x, currRow * GetFullSize() + initialPos.y, 0);
-      instance.transform.rotation = currentRotation;
+      ConfigSpriteRenderer(instance.spriteRenderer);
+      instance.spriteRenderer.transform.position = new Vector3(currCol * GetFullSize() + GetFullSize() / 2 + initialPos.x, currRow * GetFullSize() + GetFullSize() / 2 + initialPos.y, 0);
+      instance.spriteRenderer.transform.rotation = currentRotation;
       currCol = (currCol + 1) % colCount;
       if (currCol == 0) {
         currRow = (currRow + 1) % rowCount;
@@ -227,23 +274,17 @@ public class MainMenuBackground : MonoBehaviour {
   void ConfigSpriteRenderer(SpriteRenderer instance) {
     instance.drawMode = SpriteDrawMode.Sliced;
     var spriteWithMeteData = getRandomSpriteWithMeteData();
-    instance.sprite = spriteWithMeteData.Item1;
-    instance.size = spriteWithMeteData.Item2;
+    instance.sprite = spriteWithMeteData.sprite;
+    instance.size = spriteWithMeteData.sizeData;
     instance.sortingOrder = orderInLayer;
   }
-int limiter = 0;
   void Update() {
     Transform t;
     var colCounter = 0;
     var rowCounter = 0;
     foreach (var instance in instances) {
-      t = instance.transform;
-      
-      // For debugging this is convient
-      if (colCounter == 0 && limiter % 60 == 0) {
-        limiter = 0;
-      }
-      limiter++;
+      t = instance.spriteRenderer.transform;
+
       t.Rotate(Vector3.forward, RotationSpeed * Time.deltaTime);
       currentRotation = t.rotation;
       switch (pattern) {
@@ -260,16 +301,30 @@ int limiter = 0;
           StepsPattern(t);
           break;
         case Patterns.CIRCLE:
-          CircleJerk(t);
+          Circle(t, instance);
           break;
         default:
          throw new Exception("unhandled case");
       }
+
       colCounter++;
       if (colCount == (colCounter / (rowCounter + 1))) {
         rowCounter++;
       }
-      handleInstanceBounds(t, instance, GetFullSize());
+      switch (pattern) {
+        case Patterns.DEFAULT:
+        case Patterns.DIAGONAL:
+        case Patterns.ZIGZAG:
+        case Patterns.STEPS: {
+          handleInstanceBounds(t, instance.spriteRenderer, GetFullSize());
+          break;
+        }
+        case Patterns.CIRCLE: {
+          break;
+        }
+        default:
+          throw new Exception("unhandled switch case");
+      }
     }
 
     void DefaultPattern(Transform t) {
@@ -327,36 +382,68 @@ int limiter = 0;
     }
   }
 
-  void CircleJerk(Transform t) {
+  void Circle(Transform t, Instance instance) {
     var cur = _grid.WorldToCell(t.position);
     int curRow = cur.y;
     int curCol = cur.x;
-    if (curCol == -colCount / 2  && curRow != rowCount / 2) {
-      t.position += Vector3.up * Time.deltaTime * MovementSpeedY;
-    } else if (curCol == colCount / 2 && curRow != -rowCount / 2 ) {
-      t.position += Vector3.down * Time.deltaTime * MovementSpeedY;
-    } else if (curRow == -rowCount / 2  && curCol != -colCount / 2 ) {
-      t.position += Vector3.left * Time.deltaTime * MovementSpeedY;
-    } else if (curRow == rowCount / 2 && curCol != colCount / 2) {
-      t.position += Vector3.right * Time.deltaTime * MovementSpeedY;
-    } 
 
-    // Almost using grid    
-    // var cur = _grid.WorldToCell(t.position);
-    // int curRow = cur.y;
-    // int curCol = cur.x;
-    // if (curCol == -1 && curRow != rowCount -1) {
-    //   t.position += Vector3.up * Time.deltaTime * MovementSpeedY;
-    // } 
-    // if (curCol == colCount - 1 && curRow != -1) {
-    //   t.position += Vector3.down * Time.deltaTime * MovementSpeedY;
-    // } 
-    // if (curRow == -1 && curCol != -1) {
-    //   t.position += Vector3.left * Time.deltaTime * MovementSpeedY;
-    // } 
-    // if (curRow == rowCount - 1 && curCol != colCount - 1) {
-    //   t.position += Vector3.right * Time.deltaTime * MovementSpeedY;
-    // } 
+    int dFromLeft = curCol;
+    int dFromRight = colCount - curCol - 1;
+    int dFromTop = rowCount - curRow - 1;
+    int dFromBottom = curRow;
+
+
+    int middleRow = rowCount / 2;
+    
+    // if (dFromTop == dFromBottom && dFromRight == dFromLeft) {
+    //   return;
+    // }§
+
+    int col = Math.Min(dFromLeft, dFromRight);
+    int row = Math.Min(dFromTop, dFromBottom);
+    int n = Math.Min(col, row);
+
+    float absSpeed = Mathf.Abs(MovementSpeedY);
+
+    if (instance.leaving) {
+      if (n % 2 == 0) {
+        if (curCol == n && curRow != rowCount - 1 - n) {
+          t.position += Vector3.up * Time.deltaTime * absSpeed;
+        } 
+        if (curCol == colCount - 1 - n && curRow != n) {
+          t.position += Vector3.down * Time.deltaTime * absSpeed;
+        } 
+        if (curRow == n && curCol != n) {
+          t.position += Vector3.left * Time.deltaTime * absSpeed;
+        } 
+        if (curRow == rowCount - 1 - n && curCol != colCount - 1 - n) {
+          t.position += Vector3.right * Time.deltaTime * absSpeed;
+        }
+      } else {
+        if (curCol == n && curRow != n) {
+          t.position += Vector3.down * Time.deltaTime * absSpeed;
+        } 
+        if (curCol == colCount - 1 - n && curRow != rowCount - 1 - n) {
+          t.position += Vector3.up * Time.deltaTime * absSpeed;
+        } 
+        if (curRow == n && curCol != colCount - n - 1) {
+          t.position += Vector3.right * Time.deltaTime * absSpeed;
+        } 
+        if (curRow == rowCount - 1 - n && curCol !=  n) {
+          t.position += Vector3.left * Time.deltaTime * absSpeed;
+        }
+      }
+
+      var newCell = _grid.WorldToCell(t.position);
+      if (newCell.x != cur.x || newCell.y != cur.y) {
+        instance.leaving = false;  
+      }
+    } else {
+      t.position = Vector2.MoveTowards(t.position, _grid.GetCellCenterWorld(cur), Time.deltaTime * absSpeed);
+      if (Vector2.Distance(t.position, _grid.GetCellCenterWorld(cur)) < Time.deltaTime * absSpeed) {
+        instance.leaving = true;
+      }
+    }
   }
 
     void handleInstanceBounds(Transform t, SpriteRenderer instance, float fullSize) {
