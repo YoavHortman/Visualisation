0a63b6fcd1c5adca623cfcd7e360ebf32a41a46b
Yoav <Yoavhortman5@gmail.com>
2023-12-24 16:33:29 +0000

    new pattern

--- Assets/Scripts/PatternUtils.cs
+++ Assets/Scripts/PatternUtils.cs
@@ -5,99 +5,121 @@ using System.Linq;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
-public struct Sizes {
-  public float spriteSize;
-  public float borderPadding;
+public struct Sizes
+{
+    public float spriteSize;
+    public float borderPadding;
 
-  public Sizes(float spriteSize, float borderPadding) {
-    this.spriteSize = spriteSize;
-    this.borderPadding = borderPadding;
-  }
-
-  public float Sum() {
-    return spriteSize + borderPadding;
-  }
-
-  public Sizes Lerp(Sizes sizes, float time, float tolerance) {
-    var nextSize = Mathf.Lerp(spriteSize, sizes.spriteSize, time);
-    var nextBorder = Mathf.Lerp(borderPadding, sizes.borderPadding, time);
-    if (Mathf.Abs(spriteSize - sizes.spriteSize) < tolerance) {
-      nextSize = Mathf.MoveTowards(spriteSize, sizes.spriteSize, time);
+    public Sizes(float spriteSize, float borderPadding)
+    {
+        this.spriteSize = spriteSize;
+        this.borderPadding = borderPadding;
     }
 
-    if (Mathf.Abs(borderPadding - sizes.borderPadding) < tolerance) {
-      nextBorder = Mathf.MoveTowards(borderPadding, sizes.borderPadding, time);
+    public float Sum()
+    {
+        return spriteSize + borderPadding;
     }
 
-    return new Sizes(nextSize, nextBorder);
-  }
+    public Sizes Lerp(Sizes sizes, float time, float tolerance)
+    {
+        var nextSize = Mathf.Lerp(spriteSize, sizes.spriteSize, time);
+        var nextBorder = Mathf.Lerp(borderPadding, sizes.borderPadding, time);
+        if (Mathf.Abs(spriteSize - sizes.spriteSize) < tolerance)
+        {
+            nextSize = Mathf.MoveTowards(spriteSize, sizes.spriteSize, time);
+        }
 
-  public Sizes MoveTowards(Sizes sizes, float t) {
-    return new Sizes(Mathf.MoveTowards(spriteSize, sizes.spriteSize, t),
-      Mathf.MoveTowards(borderPadding, sizes.borderPadding, t));
-  }
+        if (Mathf.Abs(borderPadding - sizes.borderPadding) < tolerance)
+        {
+            nextBorder = Mathf.MoveTowards(borderPadding, sizes.borderPadding, time);
+        }
 
-  public bool IsEqual(Sizes sizes, float tolerance) {
-    return Math.Abs(borderPadding - sizes.borderPadding) < tolerance &&
-           Math.Abs(spriteSize - sizes.spriteSize) < tolerance;
-  }
+        return new Sizes(nextSize, nextBorder);
+    }
+
+    public Sizes MoveTowards(Sizes sizes, float t)
+    {
+        return new Sizes(Mathf.MoveTowards(spriteSize, sizes.spriteSize, t),
+            Mathf.MoveTowards(borderPadding, sizes.borderPadding, t));
+    }
+
+    public bool IsEqual(Sizes sizes, float tolerance)
+    {
+        return Math.Abs(borderPadding - sizes.borderPadding) < tolerance &&
+               Math.Abs(spriteSize - sizes.spriteSize) < tolerance;
+    }
 }
 
-public static class PatternUtils {
-  public static IDictionary<string, BasePattern> allPatterns = new Dictionary<string, BasePattern> {
-    { "circle", new Circle() },
-    { "circles", new Circles() },
-    { "default", new Default() },
-    { "diagonal", new Diagonal() },
-    { "explode", new Explode() },
-    { "scatter", new Scatter() },
-    { "shake", new Shake() },
-    { "snakes", new Snakes() },
-    { "steps", new Steps() },
-    { "zigzag", new Zigzag() },
-    { "fold", new Fold() },
-    { "lines", new Lines() },
-    { "circleShake", new CircleShake() }
-  };
+public static class PatternUtils
+{
+    public static IDictionary<string, BasePattern> allPatterns = new Dictionary<string, BasePattern>
+    {
+        { "circle", new Circle() },
+        { "circles", new Circles() },
+        { "default", new Default() },
+        { "diagonal", new Diagonal() },
+        { "explode", new Explode() },
+        { "scatter", new Scatter() },
+        { "shake", new Shake() },
+        { "snakes", new Snakes() },
+        { "steps", new Steps() },
+        { "zigzag", new Zigzag() },
+        { "fold", new Fold() },
+        { "lines", new Lines() },
+        { "circleShake", new CircleShake() },
+        { "forceFromPoint", new ForceFromPoint() }
+    };
 
-  public static void SetRandomTargetsInRadius(Instance[] instances, float radius) {
-    foreach (var instance in instances) {
-      instance.targetPos = instance.spriteRenderer.transform.position +
-                           new Vector3(Random.Range(-radius, radius), Random.Range(-radius, radius), 0);
-    }
-  }
-
-  public static void ResetTargetPosTo(Instance[] instances, Vector2 to) {
-    foreach (var i in instances) {
-      i.targetPos = to;
-    }
-  }
-
-  public static void SetRandomTargetsOnGrid(Instance[] instances, Grid grid, Vector2Int colRow) {
-    ArrayList allGridPositions = new ArrayList();
-    for (var i = 0; i < colRow.x; i++) {
-      for (var j = 0; j < colRow.y; j++) {
-        allGridPositions.Add(new Vector3Int(i, j, 0));
-      }
+    public static void SetRandomTargetsInRadius(Instance[] instances, float radius)
+    {
+        foreach (var instance in instances)
+        {
+            instance.targetPos = instance.spriteRenderer.transform.position +
+                                 new Vector3(Random.Range(-radius, radius), Random.Range(-radius, radius), 0);
+        }
     }
 
-    foreach (var instance in instances) {
-      Vector3Int pos = (Vector3Int)allGridPositions[Random.Range(0, allGridPositions.Count)];
-      instance.targetPos = grid.GetCellCenterWorld(pos);
-      allGridPositions.Remove(pos);
+    public static void ResetTargetPosTo(Instance[] instances, Vector2 to)
+    {
+        foreach (var i in instances)
+        {
+            i.targetPos = to;
+        }
     }
-  }
 
-  public static bool DidReach(Transform t, Vector2 target, float minDistance) {
-    return Vector2.Distance(t.position, target) < minDistance;
-  }
+    public static void SetRandomTargetsOnGrid(Instance[] instances, Grid grid, Vector2Int colRow)
+    {
+        ArrayList allGridPositions = new ArrayList();
+        for (var i = 0; i < colRow.x; i++)
+        {
+            for (var j = 0; j < colRow.y; j++)
+            {
+                allGridPositions.Add(new Vector3Int(i, j, 0));
+            }
+        }
 
-  public static BasePattern GetRandomPattern() {
-    return allPatterns.ElementAt(Random.Range(0, allPatterns.Count)).Value;
-    // return allPatterns["circleShake"];
-  }
+        foreach (var instance in instances)
+        {
+            Vector3Int pos = (Vector3Int)allGridPositions[Random.Range(0, allGridPositions.Count)];
+            instance.targetPos = grid.GetCellCenterWorld(pos);
+            allGridPositions.Remove(pos);
+        }
+    }
 
-  public static BasePattern GetInitialPattern() {
-    return allPatterns["default"];
-  }
+    public static bool DidReach(Transform t, Vector2 target, float minDistance)
+    {
+        return Vector2.Distance(t.position, target) < minDistance;
+    }
+
+    public static BasePattern GetRandomPattern()
+    {
+        return allPatterns.ElementAt(Random.Range(0, allPatterns.Count)).Value;
+        // return allPatterns["circleShake"];
+    }
+
+    public static BasePattern GetInitialPattern()
+    {
+        return allPatterns["default"];
+    }
 }
---
+++ Assets/Scripts/Patterns/ForceFromPoint.cs
@@ -0,0 +1,85 @@
+using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
+
+public class ForceFromPoint : BasePattern
+{
+    private float _step = 0;
+    private Vector2 center;
+
+    public override bool GetShouldHandleInstanceBounds()
+    {
+        return false;
+    }
+
+    public override void AfterUpdate(Instance[] instances, Grid grid, Vector2Int colRow, Vector2 movementSpeed)
+    {
+        _step += (Time.deltaTime * movementSpeed.magnitude) * 2;
+    }
+
+    public override Sizes GetSizes()
+    {
+        return new Sizes(Random.Range(0.08f, 0.2f), Random.Range(0f, 0f));
+    }
+
+    public override void AfterSizeUpdate(Instance[] instances, Vector2Int colRow, Grid grid)
+    {
+        // pick a random cell in the grid
+        var randomCell = new Vector3Int(Random.Range(0, colRow.x), Random.Range(0, colRow.y), 0);
+        center = grid.GetCellCenterWorld(randomCell);
+    }
+
+    public override Vector2Int GetNextColAndRow(Vector2 screenSizeInWorldCoords, float fullSize)
+    {
+        var nextColCount = Mathf.CeilToInt(screenSizeInWorldCoords.x * 2 / fullSize);
+        var nextRowCount = Mathf.CeilToInt(screenSizeInWorldCoords.y * 2 / fullSize);
+        return new Vector2Int(nextColCount, nextRowCount);
+    }
+
+    public override void PrePatternChange(Transform t, Instance instance, int curCol, int curRow, int index,
+        float fullSize, Grid grid,
+        Vector2Int colRow, Vector2 movementSpeed)
+    {
+        _step = 0;
+    }
+
+    public override bool IsReadyForPatternChange(Instance[] instances, Grid grid, Vector2Int colRow,
+        Vector2 movementSpeed)
+    {
+        return true;
+    }
+
+    Vector3 newPosition = Vector3.zero;
+    public override void Update(Transform t, Instance instance, int curCol, int curRow, int index,
+        float fullSize, Grid grid, Vector2Int colRow, Vector2 movementSpeed)
+    {
+        Vector2 cellCenter = grid.GetCellCenterWorld(new Vector3Int(curCol, curRow, 0));
+        float distance = Vector2.Distance(cellCenter, center) + 0.001f;
+
+        Vector2 direction = (center - cellCenter).normalized;
+
+        float oscillationX;
+        float oscillationY;
+        if (MainMenuBackground.seed >= 5)
+        {
+            oscillationX = Mathf.Sin(_step + distance) * (fullSize / 2);
+            oscillationY = Mathf.Cos(_step + distance) * (fullSize / 2);
+        }
+        else
+        {
+            oscillationX = Mathf.Sin(_step * distance) * (fullSize / 2);
+            oscillationY = Mathf.Cos(_step * distance) * (fullSize / 2);
+        }
+        
+        if (MainMenuBackground.seed % 2 == 0)
+        {
+            newPosition = cellCenter + direction * new Vector2(oscillationX, oscillationY);
+        }
+        else
+        {
+            newPosition = cellCenter + direction * new Vector2(oscillationX, oscillationX);    
+        }
+        
+        t.position = newPosition;
+    }
+}
